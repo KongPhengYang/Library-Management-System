e895aaac5bef4470bb6e458a16c8dc505485cf63
Kong Pheng Yang <144871980+KongPhengYang@users.noreply.github.com>
2025-05-07 20:18:53 -0500

    Update admin_tasks.h

--- admin_tasks.h
+++ admin_tasks.h
@@ -1,7 +1,17 @@
 #ifndef ADMIN_TASKS_H
 #define ADMIN_TASKS_H
 
-bool loginAdmin();
-void adminTasks()
+#include "Admin.h"
+#include "User.h"
+#include "Book.h"
+Admin* loginAdmin();
+
+void adminTasks(Admin* a);
+
+Book* newBook();
+void manageInventory(Admin* a);
+void manageUsers(Admin* a);
+void printStats();
+void borrowAndReturn(Admin* a);
 
 #endif
